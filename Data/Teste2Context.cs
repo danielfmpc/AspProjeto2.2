40cc9596c9fe957dcf5d59edeec5f3c782bffa84
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-12-12 12:34:27 -0300

    first commit

---
+++ Data/Teste2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Teste2.Models;
+
+namespace Teste2.Data
+{
+    public class Teste2Context : DbContext
+    {
+        public Teste2Context (DbContextOptions<Teste2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Teste2.Models.Department> Department { get; set; }
+    }
+}
