0f6a621167eda827e6279fc42bf09da4fbe4f267
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-12-18 16:48:59 -0300

    Simple Create Form

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Teste2.Models;
 using Teste2.Services;
 
 namespace Teste2.Controllers
@@ -21,6 +22,19 @@ namespace Teste2.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
